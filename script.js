0284775d2e09c64ed6b9b4bb52b9b28b9ce806f9
TobyWang1 <fufummumm@gmail.com>
2024-09-26 15:47:08 +0100

    refactor the script.js

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
 // Write your code here
-const bulb1 = document.querySelector("#lightbulb1");
-const bulb2 = document.querySelector("#lightbulb2");
-const bulb3 = document.querySelector("#lightbulb3");
-
+const bulbs = document.querySelectorAll(".lightbulb");
 const subtitle = document.querySelector(".subtitle");
 
 let count = 0;
 
-bulb1.addEventListener("click", function (){
-    count++;
-    subtitle.innerHTML = `You've clikced the light ${count} times`;
-    bulb1.classList.toggle("active")
-});
-
-bulb2.addEventListener("click", function (){
-    count++;
-    subtitle.innerHTML = `You've clikced the light ${count} times`;
-    bulb2.classList.toggle("active")
-});
-
-bulb3.addEventListener("click", function (){
-    count++;
-    subtitle.innerHTML = `You've clikced the light ${count} times`;
-    bulb3.classList.toggle("active")
+bulbs.forEach((bulb) => {
+    bulb.addEventListener("click", function() {
+        count++;
+        subtitle.innerHTML = `You've clicked the light ${count} times`;
+        bulb.classList.toggle("active");
+    })
 })
