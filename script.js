efd164c914afa6b30ccad3ee28778fe6457a7d5a
TobyWang1 <fufummumm@gmail.com>
2024-09-26 15:07:10 +0100

    initial commit with the basic instruction

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // Write your code here
+const bulb1 = document.querySelector("#lightbulb1");
+const bulb2 = document.querySelector("#lightbulb2");
+const bulb3 = document.querySelector("#lightbulb3");
+
+const subtitle = document.querySelector(".subtitle");
+
+let count = 0;
+
+bulb1.addEventListener("click", function (){
+    count++;
+    subtitle.innerHTML = `You've clikced the light ${count} times`;
+    bulb1.classList.toggle("active")
+});
+
+bulb2.addEventListener("click", function (){
+    count++;
+    subtitle.innerHTML = `You've clikced the light ${count} times`;
+    bulb2.classList.toggle("active")
+});
+
+bulb3.addEventListener("click", function (){
+    count++;
+    subtitle.innerHTML = `You've clikced the light ${count} times`;
+    bulb3.classList.toggle("active")
+})
